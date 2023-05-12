7265a528c9629e40af3b1a2f478027f17ee3c633
Roman Tentser <rtentser@yandex.ru>
2023-05-12 19:10:15 +0700

    Implement logic

--- index.js
+++ index.js
@@ -6,11 +6,79 @@ yargs(hideBin(process.argv))
   .command(
     "current",
     "Show current time",
-    () => {},
-    () => {
-      console.log(new Date());
+    (yargs) => {
+      return yargs
+        .option("date", {
+          alias: "d",
+          description: "Show current date",
+          type: "boolean",
+          conflicts: "all",
+        })
+        .option("month", {
+          alias: "m",
+          description: "Show current month",
+          type: "boolean",
+          conflicts: "all",
+        })
+        .option("year", {
+          alias: "y",
+          description: "Show current year",
+          type: "boolean",
+          conflicts: "all",
+        });
+    },
+    (argv) => {
+      const date = new Date();
+      if (argv.year) console.log(date.getFullYear());
+      else if (argv.month) console.log(date.getMonth() + 1);
+      else if (argv.date) console.log(date.getDate());
+      else console.log(date);
     }
   )
-  .command("add", "Show future time")
-  .command("sub", "Show past time")
+  .command(
+    "add",
+    "Show future time",
+    (yargs) => {
+      return yargs
+        .option("day", {
+          alias: "d",
+          description: "Show date in future days",
+          type: "number",
+        })
+        .option("month", {
+          alias: "m",
+          description: "Show date in future months",
+          type: "number",
+        });
+    },
+    (argv) => {
+      let date = new Date();
+      if (argv.month) date.setMonth(date.getMonth() + argv.month);
+      if (argv.day) date.setDate(date.getDate() + argv.day);
+      console.log(date);
+    }
+  )
+  .command(
+    "sub",
+    "Show past time",
+    (yargs) => {
+      return yargs
+        .option("day", {
+          alias: "d",
+          description: "Show date in past days",
+          type: "number",
+        })
+        .option("month", {
+          alias: "m",
+          description: "Show date in past months",
+          type: "number",
+        });
+    },
+    (argv) => {
+      let date = new Date();
+      if (argv.month) date.setMonth(date.getMonth() - argv.month);
+      if (argv.day) date.setDate(date.getDate() - argv.day);
+      console.log(date);
+    }
+  )
   .parse();
